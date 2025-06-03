86d5a2690787875a9626802189785066c0bd0b9b
anji103 <150421715+anji103@users.noreply.github.com>
2025-06-03 20:40:41 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "private_ip" {
+  description = "The private IP address of the EC2 instance"
+  value       = aws_instance.example.private_ip
+}
