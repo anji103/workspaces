27235abc157a951f17c087268f36b32bddd6c6db
anji103 <150421715+anji103@users.noreply.github.com>
2025-06-03 22:04:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket  = "statefilestorage8780"
-    key     = "${terraform.workspace}/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
+   
   }
 }
