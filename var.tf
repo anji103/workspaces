37329f5e5743d586feaa8533bee6ce58e1551b5b
anji103 <150421715+anji103@users.noreply.github.com>
2025-06-03 20:31:42 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "myamiid" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "key-name" {
+  description = "Key name"
+  type        = string
+}
+
+variable "myinstype" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "myaz" {
+  description = "Availability zone"
+  type        = string
+}
